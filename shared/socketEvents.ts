a03917b741fd846f5cb7eacb59c5662dd61800fc
TripleAsterick <www.zheng813@gmail.com>
2024-06-06 16:13:26 -0500

    init

---
+++ shared/socketEvents.ts
@@ -0,0 +1,17 @@
+
+type mouseMove = {
+  posX: number,  
+  posY: number, 
+};
+
+type userInfo = {
+  userId: number, 
+  mouseInfo: mouseMove
+};
+
+export {
+  type mouseMove, type userInfo
+};
+
+
+
